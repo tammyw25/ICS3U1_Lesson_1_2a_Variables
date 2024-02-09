e0ca0b57a6ff876047a53cc2b6c8bbe76a4b1603
tammyw25 <tammy.wong25@ycdsbk12.ca>
2024-02-09 14:16:07 -0500

    VariableTypes.java

--- VariablesTypes.java
+++ VariablesTypes.java
@@ -8,8 +8,11 @@ class VariablesTypes extends ConsoleProgram {
     int fouls = 0;
 
     //define a string variable
+    String firstName;
     // Assign a value to firstName
+    firstName = "Tammy";
    //Print out the value of the String variable
+   System.out.println(firstName);
 
     
   }
